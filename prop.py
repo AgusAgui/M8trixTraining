5d5e25130bd54e9b560a5b277b244c8a9203ceb2
Agustin <agustinguilar4142.0@gmail.com>
2026-01-01 20:32:48 -0800

    sorting card algorithm to make finding ranked hands way easier

--- prop.py
+++ prop.py
@@ -13,9 +13,72 @@ def draw(drawn, deck):
     drawn.append(num) #no real need to shuffle, we can just clear drawn to get a new set of cards
     return deck[num]
 
+def sort_cards(final_cards):
+    #insertion sort to not interfere with suites
+    sorted_cards = []
+    for rank in numbers:
+            for card in final_cards:
+                if rank in card:
+                    sorted_cards.append(card)
+    return sorted_cards
+
+def check_pair(final_cards):
+    pair_count = 0
+    for x in numbers:
+        for y in final_cards:
+            if x in y:
+                pair_count += 1
+                if (pair_count == 2):
+                    return 1
+        pair_count = 0
+    return 0
+
+def check_two_pair(final_cards):
+    pair_count  = 0
+    pairs_count = 0
+    for x in numbers:
+        for y in final_cards:
+            if x in y:
+                pair_count += 1
+                if (pair_count == 2):
+                    pairs_count += 1
+                    if(pairs_count == 2):
+                        print("There is a two pair present")
+                        return 2
+        pair_count = 0
+    return 0
+
+def check_three(final_cards):
+    pair_count = 0
+    for x in numbers:
+        for y in final_cards:
+            if x in y:
+                pair_count += 1
+                if (pair_count == 3):
+                    print("There is a three of a kind present")
+                    return 3
+        pair_count = 0
+    return 0
+
+def check_flush(final_cards):
+    flush = 0
+    for x in suites:
+        for y in final_cards:
+            if x in y:
+                flush += 1
+                if (flush == 5):
+                    print("Flush is present")
+                    return 1
+        flush = 0
+    return 0
+
+def check_fullHouse(final_cards):
+    check_three(final_cards)
+    check_pair(final_cards)
 #set up cards
-suites = {"♠", "♦", "♣", "♥"}
-numbers = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"}
+
+suites = ["♠", "♦", "♣", "♥"]
+numbers = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
 
 deck = []
 for x in numbers:
@@ -51,6 +114,8 @@ for x in range(100):
     #river
     commu_cards.append(draw(drawn,deck))
     print(commu_cards)
+    hand = sort_cards(hand + commu_cards)
+    print(hand)
     msvcrt.getch()
     hand = []
     drawn = []
