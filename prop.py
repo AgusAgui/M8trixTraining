774919598a9287c80dfbddb4df8766b64275d469
Kennedy Nguyen <kennedynguyen2902@gmail.com>
2026-01-08 20:59:21 -0800

    updated check_three function to edit a copy instead

--- prop.py
+++ prop.py
@@ -1,5 +1,6 @@
 import random
-import msvcrt
+import copy
+# import msvcrt
 
 def draw(drawn, deck):
     repeat = True
@@ -14,13 +15,12 @@ def draw(drawn, deck):
     return deck[num]
 
 def sort_cards(final_cards):
-    #insertion sort to not interfere with suites
     sorted_cards = []
     for rank in numbers:
-            for card in final_cards:
-                if rank in card:
-                    sorted_cards.append(card)
-    return sorted_cards
+        for card in final_cards:
+            if rank in card:
+                sorted_cards.append(card)
+    return sort_cards
 
 def check_pair(final_cards):
     pair_count = 0
@@ -29,35 +29,43 @@ def check_pair(final_cards):
             if x in y:
                 pair_count += 1
                 if (pair_count == 2):
+                    print("There is a pair present")
                     return 1
         pair_count = 0
     return 0
 
-def check_two_pair(final_cards):
-    pair_count  = 0
-    pairs_count = 0
+def check_three(copied):
+    for rank in numbers:
+        matches = [card for card in copied if f" {rank}" in card]
+        if len(matches) >= 3:
+            print("There is three of a kind present")
+            removed = 0
+            new = []
+            for card in copied:
+                if f" {rank}" in card and removed < 3:
+                    removed += 1
+                else:
+                    new.append(card)
+            copied[:] = new
+            return 3
+    return 0
+
+    pair_count = 0
     for x in numbers:
-        for y in final_cards:
+        for i, y in enumerate(copied):
             if x in y:
                 pair_count += 1
-                if (pair_count == 2):
-                    pairs_count += 1
-                    if(pairs_count == 2):
-                        print("There is a two pair present")
-                        return 2
+                del copied[i]
+                if (pair_count == 3):
+                    print("There is three of a kind present")
+                    return 3
         pair_count = 0
     return 0
 
-def check_three(final_cards):
-    pair_count = 0
-    for x in numbers:
-        for y in final_cards:
-            if x in y:
-                pair_count += 1
-                if (pair_count == 3):
-                    print("There is a three of a kind present")
-                    return 3
-        pair_count = 0
+def check_fullHouse(copied):
+    if check_three(copied) and check_pair(copied):
+        print("There is a full house")
+        return 5
     return 0
 
 def check_flush(final_cards):
@@ -67,31 +75,29 @@ def check_flush(final_cards):
             if x in y:
                 flush += 1
                 if (flush == 5):
-                    print("Flush is present")
+                    print("There is a flush")
                     return 1
         flush = 0
     return 0
 
-def check_fullHouse(final_cards):
-    check_three(final_cards)
-    check_pair(final_cards)
 #set up cards
-
-suites = ["♠", "♦", "♣", "♥"]
-numbers = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+suites = {"♠", "♦", "♣", "♥"}
+numbers = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"}
 
 deck = []
 for x in numbers:
     for y in suites:
         deck.append(y + ' ' + x)
 
-players = int(input("The amount of players are..."))
+players = int(input("The amount of players is..."))
 
 #print(deck)
 drawn = []
 hand = []
 commu_cards = []
 player_cards = []
+final_cards = []
+
 for x in range(100):
     #pre-flop    
     hand.append(draw(drawn, deck))
@@ -102,7 +108,7 @@ for x in range(100):
         p_hand.append(draw(drawn, deck))
         player_cards.append(p_hand)
     print(hand)
-    msvcrt.getch()
+    # msvcrt.getch()
     #flop
     for y in range(3):
         commu_cards.append(draw(drawn,deck))
@@ -110,14 +116,22 @@ for x in range(100):
     #turn
     commu_cards.append(draw(drawn,deck))
     print(commu_cards)
-    msvcrt.getch()
+    # msvcrt.getch()
     #river
     commu_cards.append(draw(drawn,deck))
     print(commu_cards)
-    hand = sort_cards(hand + commu_cards)
-    print(hand)
-    msvcrt.getch()
+    # msvcrt.getch()
+
+    final_cards = hand + commu_cards
+    copied = copy.deepcopy(final_cards)
+    print(final_cards)
+
+    check_flush(final_cards=final_cards)
+    check_fullHouse(copied=copied)
+
     hand = []
     drawn = []
     commu_cards = []
     player_cards = []
+    final_cards = []
+    copied = []
